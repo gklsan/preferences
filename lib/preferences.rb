b9c9856dbb04f04736b5ccd65527a5a18f6b9bbd
Brenda Strech <bstrech@enphaseenergy.com>
2014-09-08 16:04:48 -0700

    Bug with preferences with type of number where it would throw an exception

--- lib/preferences.rb
+++ lib/preferences.rb
@@ -418,7 +418,7 @@ module Preferences
         preferences_changed[name] = old if preference_value_changed?(name, old, value)
       end
 
-      value = ActiveRecord::ConnectionAdapters::Column.value_to_integer(value) if preference_definitions[name].number?
+      value = value.to_i if preference_definitions[name].number?
       preferences_group(group)[name] = value
 
       value
